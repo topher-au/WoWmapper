26fdbd8fc811ee1cfda5fb720148fd2074bd1642
Topher Sheridan <lordramza@gmail.com>
2016-09-14 02:15:53 +1000

    7.1.5
    Pushed release version to 7.1.5

--- WoWmapper/Properties/AssemblyInfo.cs
+++ WoWmapper/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.1.4")]
-[assembly: AssemblyFileVersion("7.1.4")]
+[assembly: AssemblyVersion("7.1.5")]
+[assembly: AssemblyFileVersion("7.1.5")]
 [assembly: Guid("f350b30b-3b5d-4957-b046-582adc9af348")]
 
